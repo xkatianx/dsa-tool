9a64257ba72583376fb6801041f06099f0cb466a
katian <23047821+xkatianx@users.noreply.github.com>
2026-05-10 20:27:59 +0800

    Add algorithms for string processing: Gusfield, KMP, and Manacher
    - Implemented Gusfield's algorithm for substring matching with linear time complexity.
    - Added KMP algorithm for efficient substring search using LPS table.
    - Introduced Manacher's algorithm for finding the longest palindromic substring in linear time.
    - Updated VSCode settings to include 'Gusfield' and 'Manacher' in the cSpell dictionary.

---
+++ src/algorithm/manacher.ts
@@ -0,0 +1,26 @@
+/** Manacher's algorithm
+ *
+ * `return[i]` is the maximum `k` such that `str[i..i-k] == str[i..i+k]`
+ *
+ * 🚀 linear time
+ *
+ * 🤏 linear space
+ */
+export function manacher(str: string) {
+  const dp = Array.from(str).map(() => 1);
+  // left: which scans to the rightmost
+  // right: the rightmost - 1
+  let [left, right] = [0, 0];
+  for (let i = 1; i < str.length; i++) {
+    // `dp[left * 2 - i]` has the result but at most `right - i + 1`
+    // biome-ignore lint/style/noNonNullAssertion: see above
+    if (i <= right) dp[i] = Math.min(right - i + 1, dp[left * 2 - i]!);
+    // either stop immediately or scanning new chars
+    // biome-ignore lint/style/noNonNullAssertion: i < str.length
+    while (str[i - dp[i]!] === (str[i + dp[i]!] ?? "xx")) dp[i]++;
+    // update rightmost
+    // biome-ignore lint/style/noNonNullAssertion: i < str.length
+    if (i + dp[i]! - 1 > right) [left, right] = [i, i + dp[i]! - 1];
+  }
+  return dp;
+}
